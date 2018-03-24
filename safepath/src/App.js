6f5f9aea9092abfe147d33afac2e3c9e4660be4e
20rohrichtt <taylorrohrich@gmail.com>
2018-03-24 16:33:45 -0400

    working api

--- safepath/src/App.js
+++ safepath/src/App.js
@@ -17,17 +17,10 @@ class App extends Component {
     return (
       <div className="App">
         <Grid>
-          <Col md={6}> hid </Col>
+          <Col md={6}> hi </Col>
           <Col md={6}> hi </Col>
         </Grid>
         <Button> hi </Button>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
