99d2ba0c1751f52e6ea84348c47da8349a746da1
20rohrichtt <taylorrohrich@gmail.com>
2018-03-24 13:20:54 -0400

    react bootstrap working

--- safepath/src/App.js
+++ safepath/src/App.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+
+import {
+  FormGroup,
+  FormControl,
+  Grid,
+  Row,
+  Col,
+  Button
+} from "react-bootstrap";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Grid>
+          <Col md={6}> hid </Col>
+          <Col md={6}> hi </Col>
+        </Grid>
+        <Button> hi </Button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
