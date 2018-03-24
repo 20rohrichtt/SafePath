ee2acf464a5eeb8e5fe4c1fb94dbc7fd3ce4d260
camlloyd2 <cvl8ec@irginia.edu>
2018-03-24 17:35:16 -0400

    pass props from App.js to findPathComponent

--- safepath/src/App.js
+++ safepath/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import FindPathComponent from "./findPathComponent";
 import Map from "./Map.js";
+import PlacesAutocomplete, { geocodeByAddress, getLatLng, geocodeByPlaceId} from 'react-places-autocomplete';
 
 import {
   FormGroup,
@@ -23,12 +24,40 @@ class App extends Component {
     super(props);
 
     this.state = {
+      showDropdown: false,
+      startAddress: "start",
+      endAddress: "end",
       paths: {
         start: { lat: 38.0314266, lng: -78.5029496 },
         end: { lat: 38.0409229, lng: -78.49572119 }
       }
     };
+    this.onStartChange = (address) => this.setState({ ...this.state, startAddress: address })
+    this.onEndChange = (address) => this.setState({ ...this.state, endAddress: address })
   }
+
+  handleDropDownChange() {
+    this.setState({
+        ...this.state,
+        showDropdown: !this.state.showDropdown
+    })
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    geocodeByAddress(this.state.startAddress)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success for start point', latLng))
+      .catch(error => console.error('Error', error))
+    
+    geocodeByAddress(this.state.endAddress)
+      .then(results => getLatLng(results[0]))
+      .then(latlng => console.log('Success for end point', latlng))
+      .catch(error => console.log('Error', error))
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -36,8 +65,14 @@ class App extends Component {
           <PageHeader>
             <h1 className="App-title">SafePath</h1>
           </PageHeader>
-
-          <FindPathComponent />
+          <FindPathComponent 
+              showDropdown = {this.state.showDropdown}
+              startAddress = {this.state.startAddress}
+              endAddress =  {this.state.endAddress}
+              onStartChange = {this.onStartChange}
+              onEndChange = {this.onEndChange}
+              handleFormSubmit = {this.handleFormSubmit}
+              handleDropDownChange = {() => this.handleDropDownChange()}/>
         </div>
         <Map
           className="Map"
--- safepath/src/findPathComponent.js
+++ safepath/src/findPathComponent.js
@@ -19,53 +19,24 @@ import "./findPathComponent.css";
 export default class findPathComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            showDropdown: false,
-            startAddress: "Start",
-            endAddress: "End",
-            
-        };  
-        this.onStartChange = (address) => this.setState({ ...this.state, startAddress: address })
-        this.onEndChange = (address) => this.setState({ ...this.state, endAddress: address })
     }
-
-    handleDropDownChange() {
-        this.setState({
-            ...this.state,
-            showDropdown: !this.state.showDropdown
-        })
-    }
-
-    handleFormSubmit = (event) => {
-        event.preventDefault()
-    
-        geocodeByAddress(this.state.startAddress)
-          .then(results => getLatLng(results[0]))
-          .then(latLng => console.log('Success for start point', latLng))
-          .catch(error => console.error('Error', error))
-        
-        geocodeByAddress(this.state.endAddress)
-          .then(results => getLatLng(results[0]))
-          .then(latlng => console.log('Success for end point', latlng))
-          .catch(error => console.log('Error', error))
-      }
     render() {
         const startInputProps = {
-            value: this.state.startAddress,
-            onChange: this.onStartChange,
+            value: this.props.startAddress,
+            onChange: this.props.onStartChange,
           }
           const endInputProps = {
-            value: this.state.endAddress,
-            onChange: this.onEndChange,
+            value: this.props.endAddress,
+            onChange: this.props.onEndChange,
           }
         return(
             <Grid style = {styles.grid}>
             
-              {this.state.showDropdown ?
+              {this.props.showDropdown ?
                <Form>
                 <Row style = {styles.row}>
                    
-                    <Button bsStyle="primary" onClick = {() => this.handleDropDownChange()} block> Find my path
+                    <Button bsStyle="primary" onClick = {this.props.handleDropDownChange} block> Find my path
                     </Button>
                 </Row>
                 <Row style = {styles.row}>
@@ -75,14 +46,14 @@ export default class findPathComponent extends Component {
                     <PlacesAutocomplete inputProps={endInputProps} />
                 </Row>
                 <Row style = {styles.row}>
-                    <Button bsStyle = "success" onClick = {this.handleFormSubmit} block> Submit</Button>
+                    <Button bsStyle = "success" onClick = {this.props.handleFormSubmit} block> Submit</Button>
                 </Row>
                     <FormControl.Feedback />
                 </Form>
                 
             : 
             <Row style = {styles.row}>
-                <Button bsStyle="primary" onClick = {() => this.handleDropDownChange()} block> Find my path
+                <Button bsStyle="primary" onClick = {this.props.handleDropDownChange} block> Find my path
                 </Button></Row>}
             </Grid>
         )
